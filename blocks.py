d16a91806ff0e6289c4229ee902ba1d7c779f13a
besteguney <63497613+besteguney@users.noreply.github.com>
2024-07-13 12:38:00 +0100

    helper files

---
+++ blocks.py
@@ -0,0 +1,41 @@
+import torch
+import numpy as np
+from torch import nn
+from torch.utils.tensorboard import SummaryWriter
+from tqdm.notebook import tqdm
+
+class Block(nn.Module):
+    # a repeating structure composed of two convolutional layers with batch normalization and ReLU activations
+    def __init__(self, in_ch, out_ch, activation='RELU'):
+        super().__init__()
+        self.activation = nn.ReLU() if activation == 'RELU' else nn.ELU()
+        self.block = nn.Sequential(nn.Conv2d(in_channels=in_ch, out_channels=out_ch, kernel_size=3, padding=1),
+                                   nn.ReLU(),
+                                   nn.BatchNorm2d(out_ch),
+                                   nn.Conv2d(in_channels=out_ch, out_channels=out_ch, kernel_size=3, padding=1),
+                                   self.activation)
+
+    def forward(self, x):
+        return self.block(x)
+
+class ResidualConv(nn.Module):
+    def __init__(self, input_dim, output_dim, stride, padding):
+        super(ResidualConv, self).__init__()
+
+        self.conv_block = nn.Sequential(
+            nn.BatchNorm2d(input_dim),
+            nn.ReLU(),
+            nn.Conv2d(
+                input_dim, output_dim, kernel_size=3, stride=stride, padding=padding
+            ),
+            nn.BatchNorm2d(output_dim),
+            nn.ReLU(),
+            nn.Conv2d(output_dim, output_dim, kernel_size=3, padding=1),
+        )
+        self.conv_skip = nn.Sequential(
+            nn.Conv2d(input_dim, output_dim, kernel_size=3, stride=stride, padding=1),
+            nn.BatchNorm2d(output_dim),
+        )
+
+    def forward(self, x):
+        return self.conv_block(x) + self.conv_skip(x)
