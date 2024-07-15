598b762991cadbb537d381b5c2aba0ceac08bb71
besteguney <63497613+besteguney@users.noreply.github.com>
2024-07-15 20:44:53 +0100

    Added the parameters.

---
+++ parameters.py
@@ -0,0 +1,11 @@
+#Constants
+ROOT_PATH = "ethz-cil-road-segmentation-2024"
+PATCH_SIZE = 16  # pixels per side of square patches
+VAL_SIZE = 10  # size of the validation set (number of images)
+CUTOFF = 0.25  # minimum average brightness for a mask patch to be classified as containing road
+N_ESTIMATORS = 3
+
+# Model Parameters
+N_EPOCHS = 20
+BATCH_SIZE = 4
+RESIZE = 384
