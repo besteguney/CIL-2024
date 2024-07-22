6104e38f0bf343efd1e67515bc1333e1b1be00e4
Arni <arnibjarnsteins@hotmail.com>
2024-07-22 09:45:41 +0000

    fix imports

--- parameters.py
+++ parameters.py
@@ -1,5 +1,5 @@
 #Constants
-ROOT_PATH = "ethz-cil-road-segmentation-2024"
+ROOT_PATH = "data"
 PATCH_SIZE = 16  # pixels per side of square patches
 VAL_SIZE = 10  # size of the validation set (number of images)
 CUTOFF = 0.25  # minimum average brightness for a mask patch to be classified as containing road
